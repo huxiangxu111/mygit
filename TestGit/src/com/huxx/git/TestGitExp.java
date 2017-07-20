1683fee8af748c4eb437d525e9533610cf78ac52
libing <13028592007@163.com>
2017-07-20 14:34:53 +0800

    develp branch add

--- TestGit/src/com/huxx/git/TestGitExp.java
+++ TestGit/src/com/huxx/git/TestGitExp.java
@@ -3,6 +3,7 @@ package com.huxx.git;
 public class TestGitExp {
 	//libing
 	// test
+	// develp branch add
 	private void syso() {
 		// TODO Auto-generated method stub
 		System.out.println("111");
